09a6b6507b387a248626a1f5042cbc8655849876
ffeaugas <feaugas.francis@gmail.com>
2023-01-05 15:25:39 +0100

    Wrote parse_quote function and its utests

--- include/minishell/parser.h
+++ include/minishell/parser.h
@@ -6,13 +6,16 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:44:19 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/05 13:45:10 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/05 14:50:49 by ffeaugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
+#include "minishell/token.h"
+#include "libft.h"
 
+t_success   my_parse_quote(t_token *toks);
 
 #endif //PARSER_H
--- src/parser/parse_quote.c
+++ src/parser/parse_quote.c
@@ -6,15 +6,54 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:41:29 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/05 13:42:53 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/05 14:51:55 by ffeaugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell/parse.h"
+#include "minishell/parser.h"
 
 #include "minishell/token.h"
+#include "libft.h"
+
+static t_success	loc_parse_doublequote(t_token *toks);
+static t_success	loc_parse_singlequote(t_token *toks);
 
 /**
  *	switch types tokens surrounded by quotes to word
  */
-t_token	*my_parse_quote(t_token *toks);
+t_success			my_parse_quote(t_token *toks)
+{
+	while (toks != NULL)
+	{
+		if (toks->type == SINGLEQUOTE)
+			return (loc_parse_singlequote(toks->next));
+		else if (toks->type == DOUBLEQUOTE)
+			return (loc_parse_doublequote(toks->next));
+		toks = toks->next;
+	}
+	return (success);
+}
+
+static t_success	loc_parse_doublequote(t_token *toks)
+{
+	while (toks != NULL)
+	{
+		if (toks->type == DOUBLEQUOTE)
+			return (my_parse_quote(toks->next));
+		toks->type = WORD;
+		toks = toks->next;
+	}
+	return (failure);
+}
+
+static t_success	loc_parse_singlequote(t_token *toks)
+{
+	while (toks != NULL)
+	{
+		if (toks->type == SINGLEQUOTE)
+			return (my_parse_quote(toks->next));
+		toks->type = WORD;
+		toks = toks->next;
+	}
+	return (failure);
+}

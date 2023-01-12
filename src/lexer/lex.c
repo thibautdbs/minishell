1f759d64aa7996d5492c0a34e8bf44eb4e6ecccc
tdubois <tdubois@student.42angouleme.fr>
2023-01-12 20:05:45 +0100

    Readability improvements on lexer fcts.

--- src/lexer/lex.c
+++ src/lexer/lex.c
@@ -6,7 +6,7 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 09:31:47 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/12 16:16:45 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/12 20:05:18 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@
 static t_maybe_toks	loc_extract_tok(char const **str);
 static void			loc_register_meta_chars_lexers(t_lexer **lexers);
 
+/** Converts string command into lexical tokens.
+ *  
+ *	Returns t_toks list or error(unmatched quotes, failed memory allocation).
+ */
 t_maybe_toks	my_lex(char const *str)
 {
 	t_toks			*toks;
@@ -41,17 +45,26 @@ t_maybe_toks	my_lex(char const *str)
 	return ((t_maybe_toks){.err = NO_ERR, .toks = toks});
 }
 
+/** Extract one token from str.
+ *
+ *  Chooses appropriate lexer for the task based on first char of str.
+ */
 static t_maybe_toks	loc_extract_tok(char const **str)
 {
 	char const	*meta_chars = "()<>&|$* \t\"\'";
 	t_lexer		*meta_chars_lexers[CHAR_MAX];
+	t_lexer		*lexer;
 
 	loc_register_meta_chars_lexers(meta_chars_lexers);
-	if (ft_strchr(meta_chars, **str) != NULL)
-		return (meta_chars_lexers[(int)**str](str));
-	return (my_lex_word(str));
+	if (ft_strchr(meta_chars, *str[0]) != NULL)
+		lexer = meta_chars_lexers[(int)*str[0]];
+	else
+		lexer = my_lex_word;
+	return (lexer(str));
 }
 
+/** Fills look up table with lexer function ptrs.
+ */
 static void	loc_register_meta_chars_lexers(t_lexer **look_up_tab)
 {
 	look_up_tab['('] = my_lex_lpar;

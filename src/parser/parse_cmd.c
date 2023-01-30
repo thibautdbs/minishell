f30d95e331491bff8906b46594abe7422be4f8be
tdubois <tdubois@student.42angouleme.fr>
2023-01-30 01:07:41 +0100

    wip

--- src/parser/parse_cmd.c
+++ src/parser/parse_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 16:31:44 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/27 17:57:31 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/28 02:32:38 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,15 @@ t_cmdlst	*my_parse_cmd(char const **pstr)
 	t_cmdlst	*cmd;
 	t_redirlst	*leading_redirs;
 
-	if (my_tok_is_redir(pstr))
+	if (my_tok_is_control_operator(*pstr))
+		return (NULL);
+	if (my_tok_is_redir(*pstr))
 	{
-		leading_redir = loc_parse_redirs(pstr);
-		if (leading_redir == NULL)
-		{
-			my_cmdlst_del(&cmd);
+		leading_redirs = loc_parse_redirs(pstr);
+		if (leading_redirs == NULL)
 			return (NULL);
-		}
-		my_redir_add_back(&cmd->redirs, leading_redir);
 	}
-	if (my_tok_type(*pstr) == LPAR)
+	if (my_tok_is_lpar(*pstr))
 		cmd = loc_parse_subshell(pstr);
 	else
 	 	cmd = loc_parse_simple_cmd(pstr);
@@ -52,15 +50,62 @@ t_cmdlst	*my_parse_cmd(char const **pstr)
 
 static t_redirlst	*loc_parse_redirs(char const **pstr)
 {
-//parse all redirs
+	t_redirlst	*lst;
+	t_redirlst	*new_redir;
+
+	lst == NULL;
+	while (my_tok_is_redir(*pstr))
+	{
+		new_redir = my_parse_redir(pstr);
+		if (new_redir == NULL)
+		{
+			my_redirlst_del(&lst);
+			return (NULL);
+		}
+		my_redirlst_add_back(&lst, new_redir);
+	}
+	return (lst);
 }
 
-static t_redirlst	*loc_parse_subshell(char const **pstr)
+static t_cmdlst	*loc_parse_subshell(char const **pstr)
 {
 //parse all redirs
 }
 
-static t_redirlst	*loc_parse_simple_cmd(char const **pstr)
+static t_cmdlst	*loc_parse_simple_cmd(char const **pstr)
 {
-//parse all redirs
+	t_cmdlst	*cmd;
+	t_wordlst	*new_word;
+	t_redirlst	*new_redir;
+
+	cmd == my_cmdlst_new(SIMPLECMD);
+	if (cmd == NULL)
+		return (NULL);
+	while (my_tok_is_control_operator(*pstr))
+	{
+		if (my_tok_is_par(*pstr))
+		{
+			my_cmdlst_del(&lst);
+			return (NULL);
+		}
+		if (my_tok_is_redir(*pstr))
+		{
+			new_redir = my_parse_redir(pstr);
+			if (new_redir == NULL)
+			{
+				my_cmdlst_del(&cmd);
+				return (NULL);
+			}
+			my_redirlst_add_back(&cmd->redirs, new_redir);
+			continue ;
+		}
+		new_word = my_parse_word(pstr);
+		if (new_word == NULL)
+		{
+			my_cmdlst_del(&cmd);
+			return (NULL);
+		}
+		my_wordlst_add_back(&cmd->words, new_word);
+	}
+	return (lst);
 }

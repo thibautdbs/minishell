4b0a18430c02aaaf55385d9392cfb52d1180c5d4
tdubois <tdubois@student.42angouleme.fr>
2023-02-13 15:30:34 +0100

    Fixed name changes.

--- src/env/envlst_init.c
+++ src/env/envlst_init.c
@@ -6,7 +6,7 @@
 /*   By: ffeaugas <ffeaugas@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 13:03:48 by ffeaugas          #+#    #+#             */
-/*   Updated: 2023/02/06 11:40:56 by ffeaugas         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:25:39 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include <stddef.h> //NULL
 
-t_envlst	*my_envlst_init(char	**envp)
+t_envlst	*my_envlst_init(char **envp)
 {
 	t_envlst	*env;
 	t_envlst	*curr;
@@ -24,10 +24,10 @@ t_envlst	*my_envlst_init(char	**envp)
 	env = NULL;
 	while (envp[i] != NULL)
 	{
-		curr = my_envlst_create(envp[i]);
+		curr = my_envlst_new(envp[i]);
 		if (curr == NULL)
 		{
-			my_envlst_destroy(&env);
+			my_envlst_del(&env);
 			return (NULL);
 		}
 		my_envlst_addback(&env, curr);

cf8ae63848d72a8c66646b34f9febe1e9ee905a8
tdubois <tdubois@student.42angouleme.fr>
2023-03-26 21:26:35 +0200

    fixed segfault in cd builtin on unsettled HOME var

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: ffeaugas <ffeaugas@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 16:23:38 by ffeaugas          #+#    #+#             */
-/*   Updated: 2023/03/21 11:14:10 by ffeaugas         ###   ########.fr       */
+/*   Updated: 2023/03/26 21:22:28 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ static	int	loc_update_env(char const *old_pwd, t_envlst **penvlst);
 
 int	my_builtin_cd(t_wordlst *words, t_envlst **penvlst)
 {
-	char	old_pwd[PATH_MAX];
-	char	pwd[PATH_MAX];
+	char	cwd[PATH_MAX];
+	char	*dst;
 
 	if (my_wordlst_size(words) > 2)
 	{
@@ -34,19 +34,21 @@ int	my_builtin_cd(t_wordlst *words, t_envlst **penvlst)
 		return (1);
 	}
 	if (my_wordlst_size(words) == 1)
-		ft_strlcpy(pwd, my_envlst_get_value("HOME", *penvlst), PATH_MAX);
+		dst = my_envlst_get_value("HOME", *penvlst);
 	else
-		ft_strlcpy(pwd, my_wordlst_at(words, 1)->content, PATH_MAX);
+		dst = my_wordlst_at(words, 1)->content;
+	if (dst == NULL)
+		return (0);
 	errno = 0;
-	if (getcwd(old_pwd, PATH_MAX) == NULL || chdir(pwd) == -1)
+	if (getcwd(cwd, PATH_MAX) == NULL || chdir(dst) == -1)
 	{
 		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-		ft_putstr_fd(my_wordlst_at(words, 1)->content, STDERR_FILENO);
+		ft_putstr_fd(dst, STDERR_FILENO);
 		ft_putstr_fd(": ", STDERR_FILENO);
 		perror("");
 		return (1);
 	}
-	return (loc_update_env(old_pwd, penvlst));
+	return (loc_update_env(cwd, penvlst));
 }
 
 static	int	loc_update_env(char const *old_pwd, t_envlst **penvlst)
